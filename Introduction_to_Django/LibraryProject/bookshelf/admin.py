6e0f8a98f1c120a659d61976198bd965672a1ec0
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-07-13 18:54:07 +0100

    Utilizing the Django Admin Interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_view = ('title', 'author', 'publication_year')
-    list_filters = ('title', 'author')
+    search_fields = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
