bd95991e64c88da8c31588b5b7c81bf6f0050410
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-07-20 15:09:48 +0100

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from django.views.generic.detail import DetailView
 
 
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
