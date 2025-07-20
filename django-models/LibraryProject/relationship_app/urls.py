f1833e96591962c6753e6d8478f5f9769aae90c4
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-07-20 15:07:53 +0100

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,7 +3,22 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 
+from django.urls import path
+from .views import register_view, login_view, logout_view, list_books, LibraryDetailView
+
+
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
+
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    
+    # Auth routes
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Library, Book
 from django.views.generic.detail import DetailView
 
+
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.select_related('author').all()
@@ -18,3 +25,40 @@ class LibraryDetailView(DetailView):
 
     def get_queryset(self):
         return Library.objects.prefetch_related('book_set__author')
+
+
+
+# User Registration View
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Auto-login after registration
+            messages.success(request, 'Registration successful.')
+            return redirect('list_books')  # Redirect to any view (like book list)
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
+# User Login View
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('list_books')
+        else:
+            messages.error(request, 'Invalid credentials.')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+
+# User Logout View
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
