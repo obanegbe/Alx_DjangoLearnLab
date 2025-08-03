d5ff99cb3686fc7f802905ad1c50386ad1e2e5f3
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-08-03 18:46:23 +0100

    Implementing Authentication and Permissions in Django REST Framework

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # Optional, for browsable API login
+    ],
+}
+
