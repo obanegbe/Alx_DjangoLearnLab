07d58e1b1e9987634960272894aa68379e935c47
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-08-10 22:33:20 +0100

    create advanced api project

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
-    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
