0c2ae0225782647f461059395c2ed6ce3e74092d
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-08-10 19:28:57 +0100

    Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- advanced_api_project/app/admin.py
+++ advanced_api_project/app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Author, Book
+
+# Register models so they can be managed via the Django admin site
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publication_year', 'author')
+    list_filter = ('publication_year', 'author')
