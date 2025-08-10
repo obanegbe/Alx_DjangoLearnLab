3544f1adf45fe485f33b9c0ece76ba6fbae22b95
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-08-10 21:11:31 +0100

    Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- advanced_api_project/advanced_api_project/settings.py
+++ advanced_api_project/advanced_api_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-nr8-9m9vc@3bokcawptbhljd-hlb%jc^d6vr%*b$!0t8%!)ptc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
