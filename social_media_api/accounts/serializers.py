f53c46d325b8d22e8d010823515ddb7da6a72c62
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-08-23 21:42:19 +0100

    Fix: add social_media_api as normal folder instead of submodule

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,28 +1,49 @@
+# accounts/serializers.py
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from .models import User
 
-User = get_user_model()
+from rest_framework.authtoken.models import Token, 
+
+Token.objects.create
+get_user_model().objects.create_user
 
 
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
 
     class Meta:
         model = User
-        fields = ["id", "username", "email", "password", "bio", "profile_picture"]
+        fields = ["username", "email", "password", "bio"]
 
     def create(self, validated_data):
-        # Create user with hashed password
-        user = User.objects.create_user(
-            username=validated_data["username"],
-            email=validated_data.get("email"),
-            password=validated_data["password"],
-            bio=validated_data.get("bio", ""),
-            profile_picture=validated_data.get("profile_picture", None),
-        )
-
-        # Generate auth token for the user
-        Token.objects.create(user=user)
-
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        user = authenticate(username=attrs.get("username"), password=attrs.get("password"))
+        if not user:
+            raise serializers.ValidationError("Invalid credentials.")
+        attrs["user"] = user
+        return attrs
+
+
+class UserSerializer(serializers.ModelSerializer):
+    followers_count = serializers.IntegerField(source="followers.count", read_only=True)
+    following_count = serializers.IntegerField(source="following.count", read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "id", "username", "email", "bio", "profile_picture",
+            "followers_count", "following_count",
+        ]
+        read_only_fields = ["id", "followers_count", "following_count"]
